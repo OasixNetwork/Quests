3c8732f2221cad7eada0e3faf3e188ef9ce1d03f
David Villarreal <contacto@yosoyvillaa.com>
2024-12-30 17:59:27 +0100

    feat: add RivalHarvesterHoes quest type

---
+++ bukkit/src/main/java/com/leonardobishop/quests/bukkit/tasktype/type/dependent/RivalHarvesterHoesBlockBreakTaskType.java
@@ -0,0 +1,69 @@
+package com.leonardobishop.quests.bukkit.tasktype.type.dependent;
+
+import com.leonardobishop.quests.bukkit.BukkitQuestsPlugin;
+import com.leonardobishop.quests.bukkit.tasktype.BukkitTaskType;
+import com.leonardobishop.quests.bukkit.util.TaskUtils;
+import com.leonardobishop.quests.bukkit.util.constraint.TaskConstraintSet;
+import com.leonardobishop.quests.common.player.QPlayer;
+import com.leonardobishop.quests.common.player.questprogressfile.TaskProgress;
+import com.leonardobishop.quests.common.quest.Quest;
+import com.leonardobishop.quests.common.quest.Task;
+import me.rivaldev.harvesterhoes.api.events.RivalBlockBreakEvent;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+
+public class RivalHarvesterHoesBlockBreakTaskType extends BukkitTaskType {
+
+    private final BukkitQuestsPlugin plugin;
+
+    public RivalHarvesterHoesBlockBreakTaskType(BukkitQuestsPlugin plugin) {
+        super("rivalharvesterhoes_blockbreak", "YoSoyVillaa", "Break crops with hoes from the RivalHarvesterHoes plugin.");
+        this.plugin = plugin;
+
+        super.addConfigValidator(TaskUtils.useRequiredConfigValidator(this, "amount"));
+        super.addConfigValidator(TaskUtils.useIntegerConfigValidator(this, "amount"));
+        super.addConfigValidator(TaskUtils.useMaterialListConfigValidator(this, TaskUtils.MaterialListConfigValidatorMode.BLOCK, "block", "blocks"));
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onRivalBlockBreak(RivalBlockBreakEvent event) {
+        Player player = event.getPlayer();
+        if (player.hasMetadata("NPC")) {
+            return;
+        }
+
+        QPlayer qPlayer = plugin.getPlayerManager().getPlayer(player.getUniqueId());
+        if (qPlayer == null) {
+            return;
+        }
+
+        Block block = event.getCrop();
+        int eventAmount = event.getAmount();
+
+        for (TaskUtils.PendingTask pendingTask : TaskUtils.getApplicableTasks(player, qPlayer, this, TaskConstraintSet.ALL)) {
+            Quest quest = pendingTask.quest();
+            Task task = pendingTask.task();
+            TaskProgress taskProgress = pendingTask.taskProgress();
+
+            super.debug("Player rival broke block '" + block.getType() + "'", quest.getId(), task.getId(), player.getUniqueId());
+
+            if (!TaskUtils.matchBlock(this, pendingTask, block, player.getUniqueId())) {
+                super.debug("Continuing...", quest.getId(), task.getId(), player.getUniqueId());
+                continue;
+            }
+
+            int progress = TaskUtils.incrementIntegerTaskProgress(taskProgress, eventAmount);
+            super.debug("Incrementing task progress (now " + progress + ")", quest.getId(), task.getId(), player.getUniqueId());
+
+            int amount = (int) task.getConfigValue("amount");
+            if (progress >= amount) {
+                super.debug("Marking task as complete", quest.getId(), task.getId(), player.getUniqueId());
+                taskProgress.setCompleted(true);
+            }
+
+            TaskUtils.sendTrackAdvancement(player, quest, task, pendingTask, amount);
+        }
+    }
+}
